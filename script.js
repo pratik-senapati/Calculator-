237c7dc982c906bdc55020009c5b30e1f43af3f5
Pratik Senapati <90540195+CriMsonTYpo@users.noreply.github.com>
2023-01-07 21:30:12 +0530

    Finish CSS
    Add scrollbar and change padding of display

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let div=document.querySelector("#div");
 let mult=document.querySelector("#mult");
 let min=document.querySelector("#min");
 let plus=document.querySelector("#plus");
+let disp=document.querySelector(".display");
+
+disp.textContent="22222222222222222222222222222222222";
+console.log(disp.textContent);
